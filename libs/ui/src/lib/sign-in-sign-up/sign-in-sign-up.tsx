445cb38de55c86c822efe04a827a1ade341f310c
andreik <gs1994@gmail.com>
2021-09-23 18:58:44 +0300

    wip

--- libs/ui/src/lib/sign-in-sign-up/sign-in-sign-up.tsx
+++ libs/ui/src/lib/sign-in-sign-up/sign-in-sign-up.tsx
@@ -1,9 +1,9 @@
 import { AppBar, Tabs, Tab, Stack, TextField } from '@mui/material';
 import Button from '@mui/material/Button';
 import { Box } from '@mui/system';
-import { useMachine } from '@xstate/react';
+import { useInterpret, useSelector } from '@xstate/react';
 import { useMemo } from 'react';
-import { assign, createMachine } from 'xstate';
+import { assign, createMachine, MachineConfig } from 'xstate';
 import './sign-in-sign-up.module.scss';
 
 type LoginResult = 'OK' | 'INVALID_USER' | 'INVALID_PASSWORD' | 'ACCESS_DENIED';
@@ -22,71 +22,74 @@ type Event = { type: 'SIGNUP' }
   | { type: 'SIGNOUT' }
   | { type: 'REGISTER' };
 
+const machineConfig: MachineConfig<IContext, any, Event> = {
+  id: 'signInSignUp',
+  initial: 'signIn',
+  context: { },
+  states: {
+    signIn: {
+      on: {
+        SIGNUP: 'signUp',
+        AUTHENTICATE: 'authenticating',
+        UPDATE: {
+          actions: assign( (ctx, event) => ({ ...ctx, ...event.data }) )
+        }
+      }
+    },
+    authenticating: {
+      on: {
+        ERROR: 'signIn',
+        SUCCESS: 'authenticated'
+      }
+    },
+    authenticated: {
+      on: {
+        SIGNOUT: 'signIn'
+      }
+    },
+    signUp: {
+      on: {
+        SIGNIN: 'signIn',
+        REGISTER: 'registering'
+      }
+    },
+    registering: {
+      on: {
+        ERROR: 'signUp',
+        SUCCESS: 'registered'
+      }
+    },
+    registered: {
+      on: {
+        SIGNIN: 'signIn'
+      }
+    }
+  }
+};
+
 export interface SignInSignUpProps {
   onCheckUser?: (userName: string, userPassword: string) => LoginResult;
 };
 
 export function SignInSignUp(props: SignInSignUpProps) {
 
-  const [state, send] = useMachine( () => createMachine<IContext, Event>({
-    id: 'signInSignUp',
-    initial: 'signIn',
-    context: { },
-    states: {
-      signIn: {
-        on: {
-          SIGNUP: 'signUp',
-          AUTHENTICATE: 'authenticating',
-          UPDATE: {
-            actions: assign( (ctx, event) => ({ ...ctx, ...event.data }) )
-          }
-        }
-      },
-      authenticating: {
-        on: {
-          ERROR: 'signIn',
-          SUCCESS: 'authenticated'
-        }
-      },
-      authenticated: {
-        on: {
-          SIGNOUT: 'signIn'
-        }
-      },
-      signUp: {
-        on: {
-          SIGNIN: 'signIn',
-          REGISTER: 'registering'
-        }
-      },
-      registering: {
-        on: {
-          ERROR: 'signUp',
-          SUCCESS: 'registered'
-        }
-      },
-      registered: {
-        on: {
-          SIGNIN: 'signIn'
-        }
-      }
-    }
-  }) );
+  const service = useInterpret(createMachine(machineConfig));
+  const stateValue = useSelector(service, state => state.value );
+  const userName = useSelector(service, state => state.context.userName );
+  const password = useSelector(service, state => state.context.password );
 
-  const { sendSignIn, sendSignUp } = useMemo( () => ({
-    sendSignIn: () => send('SIGNIN'),
-    sendSignUp: () => send('SIGNUP'),
-  }), [send]);
-
-  const getHandleChange = (fieldName: keyof IContext) => (event: React.ChangeEvent<HTMLInputElement>) => {
-    send('UPDATE', { [fieldName]: event.target.value });
-  };
+  const { sendSignIn, sendSignUp, getHandleChange } = useMemo( () => ({
+    sendSignIn: () => service.send('SIGNIN'),
+    sendSignUp: () => service.send('SIGNUP'),
+    getHandleChange: (fieldName: keyof IContext) => (event: React.ChangeEvent<HTMLInputElement>) =>
+      service.send('UPDATE', { [fieldName]: event.target.value })
+  }), [service]);
 
   return (
     <Box sx={{ bgcolor: 'background.paper', width: 500 }}>
       <AppBar position="static">
         <Tabs
-          value={state.value}
+          value={stateValue}
           indicatorColor="secondary"
           textColor="inherit"
           variant="fullWidth"
@@ -97,7 +100,7 @@ export function SignInSignUp(props: SignInSignUpProps) {
         </Tabs>
       </AppBar>
       {
-        state.value === 'signIn'
+        stateValue === 'signIn'
         ?
           <Box p={2}>
             <Stack
@@ -106,13 +109,13 @@ export function SignInSignUp(props: SignInSignUpProps) {
             >
               <TextField
                 label="Login"
-                value={state.context.userName}
+                value={userName}
                 onChange={getHandleChange('userName')}
               />
               <TextField
                 label="Password"
                 type="password"
-                value={state.context.password}
+                value={password}
                 onChange={getHandleChange('password')}
               />
               <Button variant="contained">Login</Button>
