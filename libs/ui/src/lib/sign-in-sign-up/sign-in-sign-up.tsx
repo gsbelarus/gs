4e6baeaea7f9df2bc9a1bd5eda26b0c6ce5c6cb7
andreik <gs1994@gmail.com>
2021-09-25 21:11:18 +0300

    wip

--- libs/ui/src/lib/sign-in-sign-up/sign-in-sign-up.tsx
+++ libs/ui/src/lib/sign-in-sign-up/sign-in-sign-up.tsx
@@ -1,4 +1,4 @@
-import { AppBar, Tabs, Tab, Stack, TextField } from '@mui/material';
+import { AppBar, Tabs, Tab, Stack, TextField, Typography, Container } from '@mui/material';
 import Button from '@mui/material/Button';
 import { Box } from '@mui/system';
 import { useInterpret, useSelector } from '@xstate/react';
@@ -11,6 +11,8 @@ type LoginResult = 'OK' | 'INVALID_USER' | 'INVALID_PASSWORD' | 'ACCESS_DENIED';
 interface IContext {
   userName: string;
   password: string;
+  signedIn: boolean;
+  error: string;
 };
 
 type UpdateEvent = { type: 'UPDATE', data: Partial<IContext> };
@@ -29,7 +31,9 @@ const machineConfig: MachineConfig<IContext, any, Event> = {
   initial: 'signIn',
   context: {
     userName: '',
-    password: ''
+    password: '',
+    signedIn: false,
+    error: ''
   },
   states: {
     signIn: {
@@ -69,14 +73,38 @@ const machineConfig: MachineConfig<IContext, any, Event> = {
       }
     },
     authenticating: {
-      on: {
-        ERROR: 'signIn',
-        SUCCESS: 'authenticated'
+      invoke: {
+        id: 'getUser',
+        src: 'authUser',
+        onDone: {
+          target: 'authenticated',
+          actions: assign({
+            signedIn: (_ctx, _event) => true,
+            password: (_ctx, _event) => ''
+          })
+        },
+        onError: {
+          target: 'failure',
+          actions: assign({
+            signedIn: (_ctx, _event) => false,
+            error: (_, event) => event.data,
+            password: (_ctx, _event) => ''
+          })
+        }
       }
     },
     authenticated: {
       on: {
-        SIGNOUT: 'signIn'
+        SIGNOUT: {
+          target: 'signIn',
+          actions: assign({ signedIn: (_ctx, _evt) => false })
+        }
+      }
+    },
+    failure: {
+      on: {
+        SIGNIN: 'signIn',
+        SIGNUP: 'signUp'
       }
     },
     signUp: {
@@ -106,37 +134,78 @@ export interface SignInSignUpProps {
 export function SignInSignUp(props: SignInSignUpProps) {
 
   const service = useInterpret(createMachine(machineConfig, {
+    services: {
+      authUser: (ctx, _event) => new Promise( (resolve, reject) => setTimeout( () => {
+        if (ctx.userName === 'admin') {
+          resolve(0);
+        } else {
+          reject('Invalid user name or password!')
+        }
+      }, 1000 ) )
+    },
     actions: {
       mergeContext: assign( (ctx, evt) => ({ ...ctx, ...(evt as UpdateEvent).data }) )
     }
   }));
-  const signIn = useSelector(service, state => state.value.hasOwnProperty('signIn') );
+  const state = useSelector(service, state => state);
+  const signIn = useSelector(service, state => state.matches('signIn') );
+  const signUp = useSelector(service, state => state.matches('signUp') );
+  const ready = useSelector(service, state => state.matches('signIn.ready') );
+  const authenticated = useSelector(service, state => state.matches('authenticated') );
+  const failure = useSelector(service, state => state.matches('failure') );
   const userName = useSelector(service, state => state.context.userName );
   const password = useSelector(service, state => state.context.password );
+  const error = useSelector(service, state => state.context.error );
 
-  const { sendSignIn, sendSignUp, getHandleChange } = useMemo( () => ({
-    sendSignIn: () => service.send('SIGNIN'),
-    sendSignUp: () => service.send('SIGNUP'),
-    getHandleChange: (fieldName: keyof IContext) => (event: React.ChangeEvent<HTMLInputElement>) =>
-      service.send({ type: 'UPDATE', data: { [fieldName]: event.target.value } })
-  }), [service]);
+  const { sendSignIn, sendSignUp, sendSignOut, sendAuthenticate, sendUpdateUserName, sendUpdatePassword } = useMemo(
+    () => {
+
+      const getHandleChange = (fieldName: keyof IContext) => (event: React.ChangeEvent<HTMLInputElement>) =>
+        service.send({ type: 'UPDATE', data: { [fieldName]: event.target.value } });
+
+      return ({
+        sendSignIn: () => service.send('SIGNIN'),
+        sendSignUp: () => service.send('SIGNUP'),
+        sendSignOut: () => service.send('SIGNOUT'),
+        sendAuthenticate: () => service.send('AUTHENTICATE'),
+        sendUpdateUserName: getHandleChange('userName'),
+        sendUpdatePassword: getHandleChange('password'),
+      });
+    }, [service]);
+
+  service.nextState
 
   return (
-    <Box sx={{ bgcolor: 'background.paper', width: 500 }}>
+    <Box sx={{ bgcolor: 'background.paper', width: 500, border: '1px solid gray' }}>
+      {JSON.stringify(state.nextEvents)}
       <AppBar position="static">
         <Tabs
           indicatorColor="secondary"
           textColor="inherit"
           variant="fullWidth"
           aria-label="full width tabs example"
-          value={signIn ? 'signIn' : 'signUp'}
+          value={signIn ? 'signIn' : signUp ? 'signUp' : undefined}
         >
-          <Tab label="Sign In" value='signIn' onClick={sendSignIn} />
-          <Tab label="Sign Up" value='signUp' onClick={sendSignUp}/>
+          <Tab label="Sign In" value='signIn' disabled={!state.nextEvents.includes('SIGNIN')} onClick={sendSignIn} />
+          <Tab label="Sign Up" value='signUp' disabled={!state.nextEvents.includes('SIGNUP')} onClick={sendSignUp}/>
         </Tabs>
       </AppBar>
       {
-        signIn
+        authenticated
+        ?
+          <Container>
+            <Typography>User {userName} successfully signed in.</Typography>
+            <Button variant="contained" onClick={sendSignOut}>Sign Out</Button>
+          </Container>
+        :
+          failure
+        ?
+          <Container>
+            <Typography>Error: {error}</Typography>
+            <Button variant="contained" onClick={sendSignIn}>Try again</Button>
+          </Container>
+        :
+          signIn
         ?
           <Box p={2}>
             <Stack
@@ -146,15 +215,15 @@ export function SignInSignUp(props: SignInSignUpProps) {
               <TextField
                 label="Login"
                 value={userName}
-                onChange={getHandleChange('userName')}
+                onChange={sendUpdateUserName}
               />
               <TextField
                 label="Password"
                 type="password"
                 value={password}
-                onChange={getHandleChange('password')}
+                onChange={sendUpdatePassword}
               />
-              <Button variant="contained">Login</Button>
+              <Button variant="contained" disabled={!ready} onClick={sendAuthenticate}>Login</Button>
             </Stack>
           </Box>
         :
